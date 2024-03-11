a2656eaaccb4e96d536e1d46a54b528a7e992378
Oksana Vasyliuk <streranoksana@gmail.com>
2024-03-11 13:10:29 +0200

    Home work task 1

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-print("Hallo world!")
+num_1 = 90
+print(type(num_1))
 
+num_2 = 3,33
+print(type(num_2))
 
-print("Hallo world!")
+string = "olala"
+print(type(string))
+
+name = True
+print(type(name))
+
+lst = ['my', 'ok', 'name', 'Tom']
+print(type(lst))
+
+typle = (1, 1 , 1, 1)
+print(type(typle))
+
+dictionary = {"name": "May", "age": 45}
+print(type(dictionary))
+
+set_y = {1,2, 3,}
+print(type(set_y))
+
+print(type(None))
